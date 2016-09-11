be82f1c55844d610f549d63b0d2281399f935158
sagi <sagi@tropit.com>
2016-09-11 23:48:48 +0300

    bug fixes

--- Android/build_jar.sh
+++ Android/build_jar.sh
@@ -6,15 +6,19 @@ export UNITY_LIBS
 ANDROID_SDK_ROOT="/kankado/dev/tools/android-sdk-macosx"
 export ANDROID_SDK_ROOT
 
-BOOTCLASSPATH=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home/jre/lib
+JDK_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home
+export JDK_HOME
+
+BOOTCLASSPATH=$JDK_HOME/jre/lib
 export BOOTCLASSPATH
+
 CLASSPATH=$UNITY_LIBS:$ANDROID_SDK_ROOT/platforms/android-23/android.jar
 export CLASSPATH
 
 echo "Compiling ..."
 echo $BOOTCLASSPATH
 echo $CLASSPATH
-javac *.java -bootclasspath $BOOTCLASSPATH -classpath $CLASSPATH -d . -target 1.7 -source 1.7
+$JDK_HOME/bin/javac *.java -bootclasspath $BOOTCLASSPATH -classpath $CLASSPATH -d .
 
 echo "Manifest-Version: 1.0" > MANIFEST.MF
 
--- UnityDeeplinks.cs
+++ UnityDeeplinks.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 public class UnityDeeplinks : MonoBehaviour {
 
+	#if UNITY_IOS
+	[DllImport("__Internal")]
+	private static extern void UnityDeeplinks_init(string gameObject = null, string deeplinkMethod = null);
+	#endif
+
 	// Use this for initialization
 	void Start () {
-	
+		#if UNITY_IOS
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			UnityDeeplinks_init(gameObject.name);
+		}
+		#endif
 	}
 	
 	// Update is called once per frame
@@ -14,7 +24,12 @@ public class UnityDeeplinks : MonoBehaviour {
 	
 	}
 
-	public void onDeeplink(string deeplink) {
+	public void onDeeplink2(string deeplink) {
 		Debug.Log("onDeeplink " + deeplink);
+//		Dictionary<string, string> values = new Dictionary<string, string>();
+//		values.Add("link", deeplink);
+//		AppsFlyer.trackRichEvent("deeplink", values);
 	}
+
+
 }
