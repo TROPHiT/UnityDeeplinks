fcb7518562787f732bc88e268c0a4e8563ebdd22
sagi <sagi@tropit.com>
2016-09-15 22:03:28 +0300

    removed redundant code

--- UnityDeeplinks.cs
+++ UnityDeeplinks.cs
@@ -24,11 +24,8 @@ public class UnityDeeplinks : MonoBehaviour {
 	
 	}
 
-	public void onDeeplink2(string deeplink) {
+	public void onDeeplink(string deeplink) {
 		Debug.Log("onDeeplink " + deeplink);
-//		Dictionary<string, string> values = new Dictionary<string, string>();
-//		values.Add("link", deeplink);
-//		AppsFlyer.trackRichEvent("deeplink", values);
 	}
 
 
