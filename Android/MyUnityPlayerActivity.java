e5a18bd34f32eabc255433cd709cb03f27a8de3a
sagi <sagi@tropit.com>
2017-01-17 16:23:17 +0200

    leave debug logging in comments

--- Android/MyUnityPlayerActivity.java
+++ Android/MyUnityPlayerActivity.java
@@ -11,14 +11,14 @@ public class MyUnityPlayerActivity extends UnityPlayerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        Log.d(getClass().getName(), "onCreate " + intent.getAction() + " " + intent.getDataString());
+        // Log.d(getClass().getName(), "onCreate " + intent.getAction() + " " + intent.getDataString());
         onDeeplink(getIntent());
     }
     
     
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        Log.d(getClass().getName(), "onNewIntent " + getIntent().getAction() + " " + getIntent().getDataString());
+        // Log.d(getClass().getName(), "onNewIntent " + getIntent().getAction() + " " + getIntent().getDataString());
         onDeeplink(intent);
     }
     
@@ -26,7 +26,7 @@ public class MyUnityPlayerActivity extends UnityPlayerActivity {
     protected void onDeeplink(Intent intent) {
         if (Intent.ACTION_VIEW.equals(intent.getAction())) {
             String deeplink = intent.getDataString();
-            Log.d(getClass().getName(), "onDeeplink " + deeplink);
+            // Log.d(getClass().getName(), "onDeeplink " + deeplink);
             if (deeplink != null)
                 com.unity3d.player.UnityPlayer.UnitySendMessage(gameObject, deeplinkMethod, deeplink);
         }
